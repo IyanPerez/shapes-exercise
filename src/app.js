a244803e0c4c5324b86a04610be5b6130bdd2d61
Your Name <iyanperezmarcos@gmail.com>
2022-12-30 15:33:24 +0100

    js added (in progress)

---
+++ src/app.js
@@ -0,0 +1,11 @@
+let triangle = document.getElementsByClassName('triangle');     .addEventListener("click", click());
+let square = document.getElementsByClassName('square');
+let circle = document.getElementsByClassName('circle');
+
+function click(){
+    console.log("hola");
+}
+
+
+
+
