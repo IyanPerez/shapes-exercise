a319ae734ed25c8d7bcf1058d953a787f12daa0b
Your Name <iyanperezmarcos@gmail.com>
2022-12-31 13:22:14 +0100

    hide function added to the shapes

---
+++ src/hideFunction.js
@@ -0,0 +1,26 @@
+let triangle = document.getElementById('triangle');  
+let square = document.getElementById('square');
+let circle = document.getElementById('circle');
+
+function hideTriangle(){
+    if (triangle.style.display = 'block') {
+        triangle.style.display = 'none';
+    }
+}
+function hideSquare(){
+    if (square.style.display = 'block') {
+        square.style.display = 'none';
+    }
+}
+function hideCircle(){
+    if (circle.style.display = 'block') {
+        circle.style.display = 'none';
+    }
+} 
+
+
+
+
+
+
+
